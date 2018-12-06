4376f5b1111e609b635f58250524cd60c04b99b5
btrinite <benoit.trinite@gmail.com>
2018-12-06 15:12:12 +0100

    First commit

--- README.md
+++ README.md
@@ -2,58 +2,34 @@ Installation
 ====
 
 1. Create two channels in Slack.
-  1. Name the channel for your timeline something obvious like 'twitter_timeline'.
-  2. Name the channel for posting to Twitter something obvious like 'twitter_post'.
+  1. Name the channel for posting to Twitter something obvious like 'twitter_post'.
 2. Get a Slack bot account. Visit https://my.slack.com/services/new and under "DIY Integrations & Customizations" click the "Add" button. Once you create this bot (name it something obvious like "twitter_bot") you will get an API token.
 2. Invite the bot to the channels you created in step #1. To do that, go to each channel and, in the top
     drop-down for the channel, click "Invite others to this channel..." and select your bot created in
     step #2.
-3. Associate a Twitter account with your Slack team. This is necessary so that Slack will unfurl the tweets, that is, it will show tweet text (and images that people tweet) from this account's timeline. Visit https://my.slack.com/services/twitter and then the "Authentications" tab to set this up.
-4. Login to the Twitter website Application Management website at https://apps.twitter.com/ with the Twitter account that you want to post tweets to from Slack. (It can be a different Twitter account than the one in step #3.) Click "Create New App". Make sure to give it read and write permissions.
-5. Set your environment variables. See the section below.
-6. Start the bot! Use [PM2](https://github.com/Unitech/pm2) or [forever](https://github.com/foreverjs/forever) or something that will daemonize the bot.
+3. Login to the Twitter website Application Management website at https://apps.twitter.com/ with the Twitter account that you want to post tweets to from Slack. Click "Create New App". Then create Keys and Access tokens.
+4. Set your environment variables. See the section below.
+5. Start the bot! Use [PM2](https://github.com/Unitech/pm2) or [forever](https://github.com/foreverjs/forever) or something that will daemonize the bot.
 
-Or you could deploy it to Heroku by clicking the button:
-
-[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/sillygwailo/Slack-Twitter)
-
-**You still need to follow steps #1-6 in the above instructions before clicking the button.**
 
 Warning: All messages under 140 characters will get posted to Twitter. Only use a channel designated to post tweets from.
 
 Environment Variables
 ====
 
-This bot uses environment variables to store Slack and Twitter tokens. If you want to run this software locally, use the following as a starting point.
-
-    export SLACK_POST_CHANNEL=yourSlackChannelforPostingtoTwitterHere
-    export SLACK_TIMELINE_CHANNEL=yourSlackChannelforReadingtheTwitterTimeline
-    export SLACK_TOKEN=yourSlackTokenHere
-    export TWITTER_CONSUMER_KEY=yourTwitterConsumerKeyHere
-    export TWITTER_CONSUMER_SECRET=yourTwitterConsumerSecretHere
-    export TWITTER_ACCESS_TOKEN_KEY=yourTwitterAccessTokenKeyHere
-    export TWITTER_ACCESS_TOKEN_SECRET=yourTwitterAccessTokenSecretHere
-
-If you're deploying to Heroku, press the Deploy to Heroku button above and you can punch the tokens in the form. If you're deploying from the command line, use the following:
-
-    heroku config:set SLACK_POST_CHANNEL=yourSlackChannelforPostingtoTwitterHere
-    heroku config:set SLACK_TIMELINE_CHANNEL=yourSlackChannelforReadingtheTwitterTimeline
-    heroku config:set SLACK_TOKEN=yourSlackTokenHere
-    heroku config:set TWITTER_CONSUMER_KEY=yourTwitterConsumerKeyHere
-    heroku config:set TWITTER_CONSUMER_SECRET=yourTwitterConsumerSecretHere 
-    heroku config:set TWITTER_ACCESS_TOKEN_KEY=yourTwitterAccessTokenKeyHere
-    heroku config:set TWITTER_ACCESS_TOKEN_SECRET=yourTwitterAccessTokenSecretHere
-
+SLACK_POST_CHANNEL=twitter_post
+SLACK_TOKEN=<YOUR SLACK BOT TOKEN>
+TWITTER_CONSUMER_KEY=<YOUR TWITTER CONSUMER KEY>
+TWITTER_CONSUMER_SECRET=<YOUR TWITTER CONSUMER SECRET>
+TWITTER_ACCESS_TOKEN_KEY=<YOUR TWITTER ACCESS KEY>
+TWITTER_ACCESS_TOKEN_SECRET=<YOUR TWITTER ACCESS SECRET>
+TWITTER_HANDLE="<YOUR TWITTER HANDLE>"
+TMP_PATH=<TEMP DIRECTORY RELATIVE PATH>
 Usage
 ====
 
 1. Posting messages to the 'twitter_post' channel will evaluate whether the tweet meets Twitter's definition of 140 characters. That means that URLs are compressed down (and possibly up?) to be either 23 or 24 characters in length. You don't have to short URLs. (You don't have to in general either, not even for analytics, since http://analytics.twitter.com/ handles that now.)
-2. Faving and unfaving. Starring a tweet in Slack will fave that tweet on Twitter. Unstarring it will unfave it.
-3. Retweets in a timeline are handled by taking the URL of the retweeted tweet and saying who it was retweeted by.
-4. No replies or retweeting is currently possible in the 'twitter_post' channel. Use a client for that. In the Slack mobile app, you can specify which Twitter app to use. If you know what you're doing with Automator on Mac OS X, you can add [a service to open a URL in Tweetbot](https://github.com/sillygwailo/Open-URL-in-Tweetbot.workflow)
 
 Known Issues
 ====
 
-* A memory leak. You may have to manually restart the Node.js bot if tweets stop appearing or tweets no longer get sent from your Slack channel.
-* Starring a retweet does not currently work.
--- slack-twitter.js
+++ slack-twitter.js
@@ -1,153 +1,231 @@
-var S = require('@slack/client');
-var RtmClient = S.RtmClient;
-var WebClient = S.WebClient;
-var CLIENT_EVENTS = S.CLIENT_EVENTS;
+require('dotenv').config()
 
-var Twit = require('twit');
-var TwitterText = require('twitter-text');
-var U = require('url');
+const request = require('request').defaults({ encoding: null });
+
+const { RTMClient, WebClient, CLIENT_EVENTS } = require('@slack/client');
+
+const Twit = require('twit');
+const TwitterText = require('twitter-text');
+const URL = require('url');
+
+const path = require('path'); // part of Node, no npm install needed
+const ffmpeg = require('fluent-ffmpeg');
 
 var twitterOptions = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  twitter_handle: process.env.TWITTER_HANDLE
 }
 
-var T = new Twit(twitterOptions);
+const Twitter = new Twit(twitterOptions);
 
-var slackOptions = {
+const slackOptions = {
   token: process.env.SLACK_TOKEN,
   autoReconnect: true,
   autoMark: true,
-  post_channel: process.env.SLACK_POST_CHANNEL,
-  timeline_channel: process.env.SLACK_TIMELINE_CHANNEL
+  post_channel: process.env.SLACK_POST_CHANNEL
 }
 
-var fs = require('fs');
-var MemoryDataStore = S.MemoryDataStore;
-var rtm = new RtmClient(slackOptions.token, {
+TMP_PATH = process.env.TMP_PATH
+const fs = require('fs');
+
+const rtm = new RTMClient(slackOptions.token, {
     logLevel: 'error',
-    dataStore: new MemoryDataStore()
+    dataStore: false
   }
 );
 
-var web = new WebClient(slackOptions.token);
+const web = new WebClient(slackOptions.token);
 
-rtm.login();
+function chatReply (message, text) {
+  console.log ("Replay "+text+" to channel "+message.channel)
+  web.chat.postMessage({ channel: message.channel, text: text }).then((res) => {
+  }).catch((err) =>{
+    console.log('Error:', err);
+  });
+}
+
+
+rtm.start();
+
 rtm.on('open', function() {
+  console.log ("connected to Slack")
 
 });
 rtm.on('close', function () {
   console.log('Connection closed, retrying...');
-  rtm.reconnect();
+  rtm.disconnect();
+  // You will need to store the start options from the first time you connect and then reuse them here.
+  rtm.once('disconnected', () => rtm.start());
 });
 
-stream = T.stream('user');
+function baseName(str) {
+  if(str.lastIndexOf(".") != -1) {
+    base = str.substring(0, str.lastIndexOf("."));
+  }     
+  return base;
+}
 
-stream.on('tweet', function(tweet) {
-  var channel = rtm.dataStore.getChannelByName(slackOptions.timeline_channel);
-  if (typeof(tweet.retweeted_status) != 'undefined') {
-    web.chat.postMessage(channel.id, 'https://twitter.com/' + tweet.retweeted_status.user.screen_name + '/status/' + tweet.retweeted_status.id_str + ' RT by https://twitter.com/' + tweet.user.screen_name, function(err, res) {
-      if (err) {
-        console.log('Error:', err);
-      } else {
-        console.log('Message sent: ', res);
-      }
-    });
-    if (typeof(tweet.retweeted_status.quoted_status_id_str) != 'undefined') {
-      T.get('/statuses/show/' + tweet.retweeted_status.quoted_status_id_str, {}, function(error, quoted_tweet, response) {
-        var channel = rtm.dataStore.getChannelByName(slackOptions.timeline_channel);
-        web.chat.postMessage(channel.id, 'Quoted tweet inside RT: https://twitter.com/' + quoted_tweet.user.screen_name + '/status/' + quoted_tweet.id_str, function(err, res) {
-          if (err) {
-            console.log('Error:', err);
-          } else {
-            console.log('Message sent: ', res);
-          }
-        });
-      });
-    }
-  }
-  else {
-    if (typeof(tweet.user) != 'undefined') {
-      web.chat.postMessage(channel.id, 'https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str,
-      function(err, res) {
-        if (err) {
-          console.log('Error:', err);
-        } else {
-          console.log('Message sent: ', res);
-        }
-      });
-      if (typeof(tweet.quoted_status) != 'undefined') {
-        web.chat.postMessage(channel.id, 'Quoted tweet: https://twitter.com/' + tweet.quoted_status.user.screen_name + '/status/' + tweet.quoted_status.id_str, function(err, res) {
-          if (err) {
-            console.log('Error:', err);
-          } else {
-            console.log('Message sent: ', res);
-          }
-        });
-      }
-    }
-  }
-  channel = null;
-});
-
-stream.on('error', function(error) {
-  console.log('Twitter stream error: ' + error);
-});
-
-rtm.on('star_added', function(event) {
-  if (event.item.type == 'message') {
-    path = U.parse(event.item.message.attachments[0].author_link).path.split('/');
-    T.post('favorites/create', { id: path[3] }, function(error, data, response) {
-      if (error) {
-        console.log('Error faving tweet: ' + error);
-      }
-    });
-    path = null;
-  }
-});
-rtm.on('star_removed', function(event) {
-  if (event.item.type == 'message') {
-    path = U.parse(event.item.message.attachments[0].author_link).path.split('/');
-    T.post('favorites/destroy', { id: path[3] }, function(error, data, response) {
-      if (error) {
-        console.log('Error unfaving tweet: ' + error);
-      }
-    });
-    path = null;
-  }
-});
 rtm.on('message', function(message) {
-  the_channel = rtm.dataStore.getChannelByName(slackOptions.post_channel);
-  if (message.channel == the_channel.id && (message.subtype != 'message_changed' && message.subtype != 'bot_message' && message.subtype != 'channel_join')) {
-    fs.readdir(__dirname + '/plugins/filter', function (error, files) {
-      text = message.text;
-      files.forEach(function (file) {
-        require(__dirname + '/plugins/filter/' + file);
-      });
-      filters = require(__dirname + '/plugins/filters.js').filters;
-      filters.forEach(function(filter) {
-        text = filter.execute(text);
-      });
-      if (TwitterText.getTweetLength(text) <= 140) {
-        T.post('statuses/update', { status: text }, function(error, data, response) {
-          if (error) {
-            console.log('Posting tweet error: ' + error);
+  the_channel = "";
+  web.conversations.list({types: 'private_channel'}).then(results => {
+    the_channel = results.channels.find(conversation => (conversation.name && conversation.name == slackOptions.post_channel));
+    if (the_channel) {
+      console.log ("New post on Channel")
+      if (message.channel == the_channel.id && (message.subtype != 'message_changed' && message.subtype != 'bot_message' && message.subtype != 'channel_join')) {
+        if (message.files != undefined && message.files.length!=0) {
+          // There is an attachment
+          var text = message.text
+          if (text.length == 0) {text = message.files[0].title}
+          // Check if multiple attachment
+          if (message.files.length > 1) {
+            chatReply (message, "Only the first attachement will be posted to twitter")
           }
-        });
-      }
-      else {
-        channel = rtm.dataStore.getChannelById(message.channel);
-        web.chat.postMessage(channel.id, "The tweet was too long! Character count: " + TwitterText.getTweetLength(message.text), function(err, res) {
-          if (err) {
-            console.log('Error:', err);
-          } else {
-            console.log('Message sent: ', res);
+          // check if message too long
+          if (TwitterText.getTweetLength(text) > 140) {
+            chatReply (message, "Attachment title too long !")
           }
-        });
-        channel = null;
-      } // If message longer than 140 character
-    });
-  } // Message type.
+          // Let's get the original attachment
+          request.get(message.files[0].url_private_download,{'auth': {
+            'user': null,
+            'pass': null,
+            'bearer': slackOptions.token
+            }}, function (error, response, body) {
+              if (!error && response.statusCode == 200) {
+                  var media = message.files[0].mimetype.substr(0, message.files[0].mimetype.indexOf('\/'));
+                  if (media=="image") {
+                    data = new Buffer(body).toString('base64');
+                    Twitter.post('media/upload', { media_data: data }, function (err, data, response) {
+                      if (!err) {
+                        // now we can assign alt text to the media, for use by screen readers and
+                        // other text-based presentations and interpreters
+                        var mediaIdStr = data.media_id_string
+                        var altText = text
+                        var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+                      
+                        Twitter.post('media/metadata/create', meta_params, function (err, data, response) {
+                          if (!err) {
+                            // now we can reference the media and post a tweet (media will attach to the tweet)
+                            var params = { status: text, media_ids: [mediaIdStr] }
+                      
+                            Twitter.post('statuses/update', params, function (err, data, response) {
+                              if (!err) {
+                                chatReply (message, "Posted to twitter :heavy_check_mark:")
+                              } else {
+                                chatReply (message, ":warning: : was not able to tweet that, error :"+err)
+                              }
+                            })
+                          }
+                        })
+                      } else {
+                        console.log ("Error while posting media "+err)
+                        chatReply (message, ":warning: : was not able to tweet that, error :"+err)
+                      }
+                    })
+                  } else if (media=="video") {
+
+                    const localname = `tempVideo-${Date.now()}`+path.extname(message.files[0].url_private_download);
+                    const PATH = path.join(
+                      __dirname,
+                      `${TMP_PATH}/${localname}`
+                    );
+                    fs.writeFile(PATH, body, function(error) {
+                      var transcodedFile = `${baseName(PATH)}-720p.mp4`;
+                      ffmpeg(PATH)
+                      // Generate 720P video
+                        .output(transcodedFile)
+                        .videoCodec('libx264')  
+                        .size('?x720')
+                      .on('error', function(err) {
+                            console.log('An error occurred: ' + err.message);
+                            chatReply (message, ":warning: : was not able to tweet that, error :"+err.message)
+                          
+                      })	
+                      .on('progress', function(progress) { 
+                            console.log('... frames: ' + progress.frames);
+                          
+                      })
+                      .on('end', function() { 
+                        console.log('Finished processing'); 
+                        // step ONE
+                        Twitter.postMediaChunked({ file_path: transcodedFile }, function (err, data, response) {
+                          if (err) {
+                            console.log ("postMediaChunked returned : "+err)
+                          }
+                          const mediaIdStr = data.media_id_string;
+                          const meta_params = { media_id: mediaIdStr };
+                  
+                          //  step TWO
+                          Twitter.post('media/metadata/create', meta_params, function (err, data, response) {
+                  
+                            if (!err) {
+                  
+                              const params = { status: text, media_ids: [mediaIdStr] };
+                  
+                              // step THREE
+                              Twitter.post('statuses/update', params, function (err, tweet, response) {
+                  
+                                if (err) {
+                                  console.log ("statuses/update returned "+err)
+                                  console.log (response)
+                                  chatReply (message, ":warning: : was not able to tweet that, error :"+err)
+                                } else {
+                                  chatReply (message, "Posted to twitter :heavy_check_mark:")
+                                }
+                                fs.unlinkSync(PATH); // Deletes media from /tmp folder
+                                fs.unlinkSync(transcodedFile); // Deletes media from /tmp folder
+                  
+                              }); // end '/statuses/update'
+                  
+                            } else {
+                                console.log ("media/metadata/create returned "+err)
+                                chatReply (message, ":warning: : was not able to tweet that, error :"+err)
+      
+                            } // end if(!err)
+                  
+                          }); // end '/media/metadata/create'
+                  
+                        }); // end T.postMedisChunked   
+                          
+                      })
+                      .run();
+
+                    }); //end fs.writeFile
+                  } else {
+                    chatReply (message, ":warning: : was not able to tweet that, unepected media type")
+                  }
+              } else {
+                console.log ("Error while getting media "+error)
+              }
+          });
+        } else {
+          // This is a simple text
+          fs.readdir(__dirname + '/plugins/filter', function (error, files) {
+            text = message.text;
+            files.forEach(function (file) {
+              require(__dirname + '/plugins/filter/' + file);
+            });
+            filters = require(__dirname + '/plugins/filters.js').filters;
+            filters.forEach(function(filter) {
+              text = filter.execute(text);
+            });
+            if (TwitterText.getTweetLength(text) <= 140) {
+              Twitter.post('statuses/update', { status: text }, function(error, data, response) {
+                if (error) {
+                  console.log('Posting tweet error: ' + error);
+                }
+              });
+            }
+            else {
+              chatReply (message, "The tweet was too long! Character count: " + TwitterText.getTweetLength(message.text))
+            } // If message longer than 140 character
+          });
+        }
+      } // Message type.
+    } else {
+      console.log(`the channel named ${slackOptions.post_channel} was not found.`);
+    }
+  });
 });
