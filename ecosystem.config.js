8fb23dcd0d927e8c60950fa717899ae4cf68b3b0
btrinite <benoit.trinite@gmail.com>
2018-12-06 22:00:37 +0100

    pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,30 +22,4 @@ module.exports = {
         }
       },
     ],
-  
-    /**
-     * Deployment section
-     * http://pm2.keymetrics.io/docs/usage/deployment/
-     */
-    deploy : {
-      production : {
-        user : 'node',
-        host : '172.31.21.30',
-        ref  : 'origin/master',
-        repo : 'git@github.com:btrinite/Slack-Twitter.git',
-        path : '/home/ec2-user/Slack-Twitter',
-        'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-      },
-      dev : {
-        user : 'node',
-        host : '172.31.21.30',
-        ref  : 'origin/master',
-        repo : 'git@github.com:btrinite/Slack-Twitter.git',
-        path : '/home/ec2-user/Slack-Twitter',
-        'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-        env  : {
-          NODE_ENV: 'dev'
-        }
-      }
-    }
   };
